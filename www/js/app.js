2e6c38dc526efe5bd7b5ce97c929bea0d9799316
Christian Landgren <cl@iteam.se>
2016-06-04 16:39:52 +0200

    translation and api working together

--- www/js/app.js
+++ www/js/app.js
@@ -5,85 +5,88 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module(
-  'starter', 
-    ['ionic', 'starter.controllers', 'angular-cache', 'mighub.constants']
-  )
+  'starter',
+  ['ionic', 'starter.controllers', 'angular-cache', 'mighub.constants', 'mighub.services',  'pascalprecht.translate']
+)
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
+        cordova.plugins.Keyboard.disableScroll(true)
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault()
+      }
+    })
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider, CacheFactoryProvider) {
+    angular.extend(CacheFactoryProvider.defaults, {
+      maxAge: 15 * 60 * 1000, // 15 minutes
+      deleteOnExpire: 'aggressive',
+      onExpire: function (key, value) {
+        var _this = this; // "this" is the cache in which the item expired
+        angular.injector(['ng']).get('$http').get(key).success(function (data) {
+          _this.put(key, data)
+        })
+      }
+    })
+
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+      .state('app.categories', {
+        url: '/categories',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/categories.html',
+            controller: 'CategoriesCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: '/categories/:categoryId',
+        animate: 'slide-in-up',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/category.html',
+            controller: 'CategoryCtrl'
+          }
+        }
+      })
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/categories')
+  })
+
+angular.module('starter').config(function ($translateProvider) {
+  $translateProvider.preferredLanguage('en')
 })
-
-.config(function($stateProvider, $urlRouterProvider, CacheFactoryProvider) {
-
-  angular.extend(CacheFactoryProvider.defaults, {
-    maxAge: 15 * 60 * 1000, // 15 minutes
-    deleteOnExpire: 'aggressive',
-    onExpire: function (key, value) {
-      var _this = this; // "this" is the cache in which the item expired
-      angular.injector(['ng']).get('$http').get(key).success(function (data) {
-        _this.put(key, data);
-      });
-    }
-  });
-
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.categories', {
-      url: '/categories',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/categories.html',
-          controller: 'CategoriesCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/categories/:categoryId',
-    animate: 'slide-in-up',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/category.html',
-        controller: 'CategoryCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/categories');
-});
