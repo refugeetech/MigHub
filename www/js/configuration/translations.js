49ea646a689177d5152ca95f5b0f179b40ea7b49
Ozcino <oscar@haglind.net>
2016-06-20 21:43:46 +0200

    Adds new lables
    New translations

--- www/js/configuration/translations.js
+++ www/js/configuration/translations.js
@@ -1,37 +1,77 @@
 angular.module("localeapp.translations", []).config(["$translateProvider", function($translateProvider) {
 $translateProvider.translations("ar", {
-  "categories": " ",
-  "category_social": "Soc",
-  "choose_language": " ",
+  "categories": "الفئات",
+  "choose_language": "إختر اللغة",
+  "education_description": null,
+  "education_title": "التعليم",
   "employment_description": null,
+  "employment_title": "الشغل",
+  "housing_description": null,
+  "housing_title": "السكن",
+  "information_description": null,
+  "information_title": "معلومات",
   "interface_direction": "rtl",
-  "label": "اللغة العربية"
+  "label": "اللغة العربية",
+  "language_description": null,
+  "language_title": "لغات",
+  "social_description": null,
+  "social_title": "تواصل اجتماعي"
 });
 
 $translateProvider.translations("en", {
   "categories": "Categories",
-  "category_social": "Social",
   "choose_language": "Choose Language",
-  "employment_description": "Let's vim it live!",
+  "education_description": null,
+  "education_title": "Education",
+  "employment_description": null,
+  "employment_title": "Employment",
+  "housing_description": null,
+  "housing_title": "Housing",
+  "information_description": null,
+  "information_title": "Information",
   "interface_direction": "ltr",
-  "label": "English"
+  "label": "English",
+  "language_description": null,
+  "language_title": "Language",
+  "social_description": null,
+  "social_title": "Social"
 });
 
 $translateProvider.translations("fa", {
   "categories": " ",
-  "category_social": "Soc",
   "choose_language": " ",
+  "education_description": null,
+  "education_title": null,
   "employment_description": null,
+  "employment_title": null,
+  "housing_description": null,
+  "housing_title": null,
+  "information_description": null,
+  "information_title": null,
   "interface_direction": "rtl",
-  "label": "زبان فارسي"
+  "label": "زبان فارسي",
+  "language_description": null,
+  "language_title": null,
+  "social_description": null,
+  "social_title": null
 });
 
 $translateProvider.translations("sv", {
   "categories": "Kategorier",
-  "category_social": "Socialt",
   "choose_language": "Välj Språk",
+  "education_description": null,
+  "education_title": "Utbildning",
   "employment_description": "Jobb beskrivning",
+  "employment_title": "Arbete",
+  "housing_description": null,
+  "housing_title": "Boende",
+  "information_description": null,
+  "information_title": "Information",
   "interface_direction": "ltr",
-  "label": "Svenska"
+  "label": "Svenska",
+  "language_description": null,
+  "language_title": "Språk",
+  "social_description": null,
+  "social_title": "Socialt"
 });
 }]);
